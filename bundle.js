030b3a285235ce0fe718882a4c5cf76c593b30dd
jeffliang0318 <liangnash0318@gmail.com>
2018-04-17 09:24:46 -0700

    basic logic done

---
+++ bundle.js
@@ -0,0 +1,214 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// var canvas = document.getElementById("myCanvas");
+// var ctx = canvas.getContext("2d");
+//
+//   ctx.canvas.width  = window.innerWidth;
+//   ctx.canvas.height = window.innerHeight;
+//...drawing code...
+
+// 1= wall
+// 2=path
+// 3=hole
+// 5=ball
+var map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 1, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+
+var ball = {
+  x: 1,
+  y: 1
+};
+
+function drawMap() {
+  var board = document.getElementById("boardId");
+  board.innerHTML = "";
+  for (var j = 0; j < map.length; j++) {
+    // console.log(map[j]);
+    for (var i = 0; i < map[j].length; i++) {
+      // console.log(map[j][i]);
+      switch (map[j][i]) {
+        case 1:
+          board.innerHTML += "<div class='wall'></div>";
+          break;
+        case 2:
+          board.innerHTML += "<div class='path'></div>";
+          break;
+        case 3:
+          board.innerHTML += "<div class='hole'></div>";
+          break;
+        case 5:
+          board.innerHTML += "<div class='ball'></div>";
+          break;
+
+      }
+    }
+    board.innerHTML += "<br>";
+  }
+}
+
+document.onkeydown = function (e) {
+  switch (e.keyCode) {
+    // 1= wall
+    // 2=path
+    // 3=hole
+    // 5=ball
+    case 37:
+      // left
+      if (map[ball.y][ball.x - 1] === 2) {
+        // can't move to wall
+        map[ball.y][ball.x] = 2;
+        ball.x = ball.x - 1;
+        map[ball.y][ball.x] = 5;
+        drawMap();
+        break;
+      } else if (map[ball.y][ball.x - 1] === 3) {
+        //  ball drop into the hole
+        map[1][1] = 5;
+        map[ball.y][ball.x] = 2;
+        var _ref = [1, 1];
+        ball.x = _ref[0];
+        ball.y = _ref[1];
+
+        drawMap();
+        break;
+      }
+      break;
+
+    case 38:
+      // up
+      if (map[ball.y - 1][ball.x] === 2) {
+        map[ball.y][ball.x] = 2;
+        ball.y = ball.y - 1;
+        map[ball.y][ball.x] = 5;
+        drawMap();
+        break;
+      } else if (map[ball.y - 1][ball.x] === 3) {
+        //  ball drop into the hole
+        map[1][1] = 5;
+        map[ball.y][ball.x] = 2;
+        var _ref2 = [1, 1];
+        ball.x = _ref2[0];
+        ball.y = _ref2[1];
+
+        drawMap();
+        break;
+      }
+      break;
+    case 39:
+      // right
+      if (map[ball.y][ball.x + 1] === 2) {
+        map[ball.y][ball.x] = 2;
+        ball.x = ball.x + 1;
+        map[ball.y][ball.x] = 5;
+        drawMap();
+        break;
+      } else if (map[ball.y][ball.x + 1] === 3) {
+        //  ball drop into the hole
+        map[1][1] = 5;
+        map[ball.y][ball.x] = 2;
+        var _ref3 = [1, 1];
+        ball.x = _ref3[0];
+        ball.y = _ref3[1];
+
+        drawMap();
+        break;
+      }
+      break;
+    case 40:
+      // down
+      if (map[ball.y + 1][ball.x] === 2) {
+        map[ball.y][ball.x] = 2;
+        ball.y = ball.y + 1;
+        map[ball.y][ball.x] = 5;
+        drawMap();
+        break;
+      } else if (map[ball.y + 1][ball.x] === 3) {
+        //  ball drop into the hole
+        map[1][1] = 5;
+        map[ball.y][ball.x] = 2;
+        var _ref4 = [1, 1];
+        ball.x = _ref4[0];
+        ball.y = _ref4[1];
+
+        drawMap();
+        break;
+      }
+      break;
+
+  }
+};
+
+drawMap();
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
--- maze.js
+++ maze.js
@@ -1,22 +1,151 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+// var canvas = document.getElementById("myCanvas");
+// var ctx = canvas.getContext("2d");
+//
+//   ctx.canvas.width  = window.innerWidth;
+//   ctx.canvas.height = window.innerHeight;
+  //...drawing code...
 
-ctx.beginPath();
-ctx.rect(0 , 0, 200, 200);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+  // 1= wall
+  // 2=path
+  // 3=hole
+  // 5=ball
+let map = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,1,2,3,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+];
 
-//circle
-ctx.beginPath();
-// (updown, left/right, size , notsure, but 0 is full circle)
-ctx.arc(160, 100, 50, 0, Math.PI*2, false);
-ctx.fillStyle = "grey";
-ctx.fill();
-ctx.closePath();
+let ball = {
+  x: 1,
+  y: 1
+};
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function drawMap() {
+  let board = document.getElementById("boardId");
+  board.innerHTML = "";
+  for (var j = 0; j < map.length; j++) {
+    // console.log(map[j]);
+    for (var i = 0; i < map[j].length; i++) {
+      // console.log(map[j][i]);
+      switch (map[j][i]) {
+        case 1:
+        board.innerHTML +=
+        "<div class='wall'></div>";
+        break;
+        case 2:
+        board.innerHTML +=
+        "<div class='path'></div>";
+        break;
+        case 3:
+        board.innerHTML +=
+        "<div class='hole'></div>";
+        break;
+        case 5:
+        board.innerHTML +=
+        "<div class='ball'></div>";
+        break;
+
+      }
+    }
+    board.innerHTML += "<br>";
+  }
+}
+
+document.onkeydown = function(e){
+  switch ( e.keyCode ) {
+    // 1= wall
+    // 2=path
+    // 3=hole
+    // 5=ball
+    case 37:
+    // left
+      if (map[ball.y][ball.x - 1] === 2 ) {
+        // can't move to wall
+        map[ball.y][ball.x] = 2;
+        ball.x = ball.x - 1;
+        map[ball.y][ball.x] = 5;
+        drawMap();
+        break;
+      } else if (map[ball.y][ball.x - 1] === 3) {
+        //  ball drop into the hole
+        map[1][1] = 5;
+        map[ball.y][ball.x] = 2;
+        [ball.x, ball.y] = [1,1];
+        drawMap();
+        break;
+      }
+      break;
+
+    case 38:
+    // up
+    if (map[ball.y - 1][ball.x] === 2) {
+      map[ball.y][ball.x] = 2;
+      ball.y = ball.y - 1;
+      map[ball.y][ball.x] = 5;
+      drawMap();
+      break;
+    } else if (map[ball.y - 1][ball.x] === 3) {
+      //  ball drop into the hole
+      map[1][1] = 5;
+      map[ball.y][ball.x] = 2;
+      [ball.x, ball.y] = [1,1];
+      drawMap();
+      break;
+    }
+      break;
+    case 39:
+    // right
+    if (map[ball.y][ball.x + 1] === 2) {
+      map[ball.y][ball.x] = 2;
+      ball.x = ball.x + 1;
+      map[ball.y][ball.x] = 5;
+      drawMap();
+      break;
+    } else if (map[ball.y][ball.x + 1] === 3) {
+      //  ball drop into the hole
+      map[1][1] = 5;
+      map[ball.y][ball.x] = 2;
+      [ball.x, ball.y] = [1,1];
+      drawMap();
+      break;
+    }
+      break;
+    case 40:
+    // down
+    if (map[ball.y + 1][ball.x] === 2) {
+      map[ball.y][ball.x] = 2;
+      ball.y = ball.y + 1;
+      map[ball.y][ball.x] = 5;
+      drawMap();
+      break;
+    } else if (map[ball.y + 1][ball.x] === 3) {
+      //  ball drop into the hole
+      map[1][1] = 5;
+      map[ball.y][ball.x] = 2;
+      [ball.x, ball.y] = [1,1];
+      drawMap();
+      break;
+    }
+      break;
+
+  }
+};
+
+drawMap();
